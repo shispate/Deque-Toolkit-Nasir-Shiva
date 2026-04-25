03c03d6ed3fdc727e5bb099f48eea83053eaa036
ndavis67-hub <ndavis67@ut.utm.edu>
2026-04-25 00:31:16 +0000

    A bit of comment progress

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# Deque-Toolkit-Nasir-Shiva
-C++ deque built using a dynamic block array.
+# Deque-Toolkit
+### Authors: Shiva and Nasir
+
+## Description
+This is a block-based deque data structure that allows you to add and remove elements from the front and back efficiently O(1). The deque uses a blockmap which is an array of pointers where each pointer points to a fixed block size of elements. The goal in doing this is to prevent it from being similar to a vector and to keep the add and remove functions to an O(1) time complexity. The deque automatically grows the blockmap when it runs out of room in either direction.
+
+## Compile
+Compile the program by entering 'make' command in linux-based systems
+
+## How to run
+Run the program by entering the ./deque_toolkit command in linux-based systems
+
+## How it works
+The deque stores elements and are tracked by four pointers
+1. first_block()
+2. first_element()
+3. last_block()
+4. last_element()
+
+When a block fills up, a new one is allocated. When the blockmap fills up, it then doubles in size using 
+- growFront()
+- growBack()
+
+## Operations Used
+- **push_back(element)**
+- **push_front(element)**
+- **pop_back()**
+- **pop_front()**
+- **front()**
+- **back()**
+- **size()**
+- **empty()**
+- **operator[]** 
--- deque.h
+++ deque.h
@@ -4,39 +4,92 @@
 class Deque{
 
  private:
-  int** blockmap;
-  int first_element;
-  int last_element;
-  int first_block;
-  int last_block;
-  int capacity;
-  int mapSize;
+  int** blockmap; // array of pointers, each are pointing to a fixed block of elements
+  int first_element; //a slot within the first block and is the first element
+  int last_element; // a slot within the last block and is the last element
+  int first_block; // the first block within the blockmap, this holds the first element
+  int last_block; // the last block within the blockmap, this holds the last element
+  int capacity; // total number of block slots available in blockmap
+  int mapSize; // current number of blocks in use
   int numElements;  // total elements stored
-  const static int blockSize = 5;
+  const static int blockSize = 5; // fixed number of slots in each block
   void growFront(); // used for when blockmap runs out of room
   void growBack();  // and needs to be resized
 
  public:
+ /**
+  * @brief Construct a new Deque object
+  * 
+  */
   Deque();
 
+  /**
+   * @brief Destroy the Deque object
+   * 
+   */
   ~Deque();
 
+  /**
+   * @brief returns how many elements are currently in the deque
+   * 
+   * @return int 
+   */
   int size();
 
+  /**
+   * @brief adds an element to the back of the deque
+   * 
+   * @param element is the value to add
+   */
   void push_back(int element);
 
+  /**
+   * @brief adds an element to the front of the deque 
+   * 
+   * @param element is the value to add
+   */
   void push_front(int element);
 
+  /**
+   * @brief removes an element from the back of the deque
+   * 
+   */
   void pop_back();
 
+  /**
+   * @brief removes the element from the front of the deque
+   * 
+   */
   void pop_front();
 
+  /**
+   * @brief returns the element at the front of the deque without removing it
+   * 
+   * @return int 
+   */
   int front();
 
+  /**
+   * @brief returns the element at the back of the deque without removing it
+   * 
+   * @return int 
+   */
   int back();
 
+  /**
+   * @brief checks if the deque is empty
+   * 
+   * @return true 
+   * @return false 
+   */
   bool empty();
 
+  /**
+   * @brief Accesses an element by its index which the index would be 0 at the front. 
+   *  uses block and slot to determine the exact memory location 
+   * @param index is the logical positon from the front of the deque
+   * @return int& reference to the element at that position
+   */
   int& operator[] (unsigned int index);
   
 
